07fbca8ff90dd47cd84c6e28e1a28cd68394ead6
Jan Algermissen <algermissen@acm.org>
2020-11-03 22:52:29 +0100

    Add support for jwt

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,4 +1,7 @@
 use serde_json;
+use serde::{Serialize, Deserialize};
+use jsonwebtoken::{encode, decode, Header, Algorithm, Validation, EncodingKey, DecodingKey};
+use std::result::Result;
 
 #[derive(Clone)]
 pub struct User {
@@ -26,4 +29,28 @@ pub fn user_from_bearer(token:&str) -> Option<User> {
     //serde_json::from_str(token).map(|t:Token| User { id: t.sub.clone() , name: t.name.clone() , organisation: t.organisation.clone() })
     serde_json::from_str(token).map(|t:Token| User { id: t.sub.clone() , name: t.name.clone() , organisation: t.organisation.clone() }).ok()
 }
+
+/// Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+    sub: String,
+    // company: String,
+    exp: usize,
+}
+
+pub fn getsub(c:&Claims) -> String {
+    c.sub.clone()
+}
+
+pub fn validate(token:String) -> Result<Claims,String> {
+
+    let b = include_bytes!("pubkey.pem");
+    ///     print!("{}", String::from_utf8_lossy(bytes));
+    // let tok2 = encode(&Header::new(Algorithm::RS256), &claims, &EncodingKey::from_rsa_pem(bytes).expect("xx"));
+
+    let tok = decode::<Claims>(token.as_str(), &DecodingKey::from_rsa_pem(b).expect("sdddd"), &Validation::new(Algorithm::RS256)).expect("not valid");
+
+    Ok(tok.claims)
+}
+
 
