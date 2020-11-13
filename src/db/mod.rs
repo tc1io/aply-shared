adb855f8e8bc5ce91927c4eeb3eb9e6fac2fe661
Jan Algermissen <algermissen@acm.org>
2020-11-13 08:22:22 +0100

    Add dummy database implementation
    This adds a database object that can be instantiated once
    globally in the 'main' thread and then provides the ability
    to obtain handles that can be passed across threads.

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,4 +1,8 @@
+use super::ply;
 
+// DB connection that maintains any shared state needed by all remote
+// handles (See Handle struct). Example would ge auth tokens generated
+// for accessing eg Google Datastore API.
 pub struct Db {
     handle: Handle,
 }
@@ -10,6 +14,7 @@ pub fn new() -> Db {
 
 impl Db {
 
+    // Get a handle to the DB object that can be send across threads.
     pub fn handle(&self) -> &Handle {
         &self.handle
     }
@@ -18,15 +23,14 @@ impl Db {
 
 #[derive(Debug, Clone)]
 pub struct Handle {
-
 }
 
 impl Handle {
 
-    pub fn handle(&self) -> &Handle {
-        &self
+    pub fn get_system(&self,id: &str) ->  impl std::future::Future<Output=std::result::Result<ply::System,i32>> {
+        let system = ply::System{name:String::from(id)};
+        std::future::ready(std::result::Result::Ok(system))
     }
-
 
 }
 
